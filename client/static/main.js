c7ce36d6e8e35ae2636ae9f53cc4e0b22956c53c
Alisher Karim <alisherkarim@github.com>
2021-09-08 18:07:56 +0900

    new version of initial actions

--- client/static/main.js
+++ client/static/main.js
@@ -8,19 +8,8 @@ joinForm.addEventListener('submit', (e)=>{
     var username = document.getElementById("username").value;
     var room = document.getElementById("room").value;
     socket.emit("join room", {room: room, username: username});
-    document.getElementById("username").value = "";
-    document.getElementById("room").value = "";
-    document.getElementById("joinForm").style.display = "none";
-    document.getElementById("room_members_list").style.display = "block";
 })
 
-socket.on('your room', (room_members)=>{
-    console.log("ou yeah...");
-    document.getElementById("room_members_ul").innerHTML = "";
-    for(var i = 0; i < room_members.length; i++){
-        const li = document.createElement("li");
-        li.classList.add("list-group-item");
-        li.innerText = room_members[i].username;
-        document.getElementById("room_members_ul").appendChild(li);
-    }
+socket.on('room', (data) => {
+    $("#main").load('room', {members: data}); //.load(route, json_data) makes a post request to /route
 })
