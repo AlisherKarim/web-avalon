75ef0c301ee9374822a6fba1ff96b9ee952b2e84
Alisher Karim <alisherkarim@github.com>
2021-09-24 10:15:22 +0900

    start game button

--- client/static/main.js
+++ client/static/main.js
@@ -1,6 +1,7 @@
 const socket = io();
 
 var joinForm = document.getElementById("joinForm");
+var createForm = document.getElementById("createform");
 
 joinForm.addEventListener('submit', (e)=>{
     console.log("submit happened!");
@@ -10,6 +11,24 @@ joinForm.addEventListener('submit', (e)=>{
     socket.emit("join room", {room: room, username: username});
 })
 
-socket.on('room', (data) => {
-    $("#main").load('room', {members: data}); //.load(route, json_data) makes a post request to /route
+createForm.addEventListener('submit', (e)=>{
+    console.log("creating new room event happened!");
+    e.preventDefault(e);
+    var username = document.getElementById("create-username").value;
+    var room = "";
+    for(var i = 0; i < 6; i++){
+        room += Math.floor(Math.random() * 10).toString();
+    }
+    socket.emit("create room", {room: room, username: username});
+    // socket.emit("join room", {room: room, username: username});
+})
+
+socket.on('room', (data) => { //refreshes the room page for each member
+    /*
+    data = {
+        roomID, room (object from index.js)
+    }
+    */
+    // console.log(socket.id);
+    $("#main").load('room', {user: socket.id, room: data.room, roomID: data.roomID}); //.load(route, json_data) makes a post request to /route
 })
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ var express         = require('express');
 var app             = express();
 var server          = require('http').Server(app);
 var io              = require('socket.io')(server);
-var path            = require('path')
+var path            = require('path');
+const { exit } = require('process');
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname + "/../client"));
@@ -14,9 +15,46 @@ app.use("/static", express.static(path.resolve(__dirname + '/../client/static/')
 var users = new Array();
 var rooms = {}; //dictionary or map
 
+//===============small descriptions of user and room "objects"===========
+/*user = {
+    id: String, socketID,
+    username: String, nickName,
+    room: String, room id the user is in,
+}
+
+rooms[i] is a list of users in ith room
+rooms[i] = {
+    roomhost: String, socket-id of the person who created it
+    members: List, users in this room
+}
+
+*/
+
+
 //===============Socket IO=====================
 io.on('connection', function (socket) {
     console.log("user connected!");
+
+    socket.on("create room", (data) => {
+        rooms[data.room] = {
+            roomhost: socket.id,
+            members: []
+        };
+        users.push(
+            {
+                id: socket.id, 
+                username: data.username, 
+                room: data.room
+            }
+        )
+        rooms[data.room].members.push({
+            username: data.username,
+            id: data.room
+        });
+        socket.join(data.room);
+        io.in(data.room).emit('room', {roomID: data.room, room: rooms[data.room]});
+    })
+
     socket.on("join room", (data)=>{
         users.push(
             {
@@ -26,14 +64,15 @@ io.on('connection', function (socket) {
             }
         )
         if(!(data.room in rooms)){
-            rooms[data.room] = [];
+            console.log("join room event for non-existing room!");
+            exit();
         }
-        rooms[data.room].push({
+        rooms[data.room].members.push({
             username: data.username,
             id: socket.id
         });
         socket.join(data.room);
-        io.in(data.room).emit('room', rooms[data.room]);
+        io.in(data.room).emit('room', {roomID: data.room, room: rooms[data.room]});
     })
 
     socket.on("disconnect", ()=>{
@@ -44,12 +83,13 @@ io.on('connection', function (socket) {
         users = users.filter((user)=>{ 
             return user.id !== socket.id;
         });
+        //we have to delete the user from the room if the user existed in some specific room
         if(currentUser){
             console.log("user", currentUser.username, "disconnected from his room", currentUser.room);
-            rooms[currentUser.room] = rooms[currentUser.room].filter((user) =>{
+            rooms[currentUser.room].members = rooms[currentUser.room].members.filter((user) =>{
                 return user.id !== socket.id;
             });
-            io.in(currentUser.room).emit('your room', rooms[currentUser.room]);
+            io.in(currentUser.room).emit('room', {room: rooms[currentUser.room], roomID: currentUser.room});
         }
     })
 })
@@ -67,8 +107,8 @@ app.get("/game", (req, res) =>{
 
 //===============POST requests==================
 app.post("/room", (req, res) => {
-    // console.log(req.body.members);
-    res.render('room', {members: req.body.members});
+    // console.log(req.body);
+    res.render('room', {room: req.body.room, roomID: req.body.roomID, user: req.body.user});
 })
 
 //===============Server LISTENS=================
