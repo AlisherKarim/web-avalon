8ac43ffd2f639807a0d9303469a05d9a64e8dba6
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2021-09-29 22:16:11 +0900

    implemented showRoles

--- client/static/main.js
+++ client/static/main.js
@@ -3,7 +3,7 @@ const socket = io();
 var joinForm = document.getElementById("joinForm");
 var createForm = document.getElementById("createform");
 
-joinForm.addEventListener('submit', (e)=>{
+joinForm.addEventListener('submit', (e) => {
     console.log("submit happened!");
     e.preventDefault(e);
     var username = document.getElementById("username").value;
@@ -11,16 +11,17 @@ joinForm.addEventListener('submit', (e)=>{
     socket.emit("join room", {room: room, username: username});
 })
 
-createForm.addEventListener('submit', (e)=>{
-    console.log("creating new room event happened!");
+createForm.addEventListener('submit', (e) => {
     e.preventDefault(e);
     var username = document.getElementById("create-username").value;
+    
     var room = "";
     for(var i = 0; i < 6; i++){
         room += Math.floor(Math.random() * 10).toString();
     }
+    
+    console.log("creating new room event happened!");
     socket.emit("create room", {room: room, username: username});
-    // socket.emit("join room", {room: room, username: username});
 })
 
 socket.on('room', (data) => { //refreshes the room page for each member
@@ -29,6 +30,18 @@ socket.on('room', (data) => { //refreshes the room page for each member
         room (object from index.js)
     }
     */
-    // console.log(socket.id);
-    $("#main").load('room', {user: socket.id, room: data.room}); //.load(route, json_data) makes a post request to /route
+   console.log("<main.js> socket.on('room')");
+   $("#main").load('room', {user: socket.id, room: data.room}); //.load(route, json_data) makes a post request to /route
+})
+
+
+
+socket.on("gameStart", (data) => {
+    /**
+     * data = {
+     *  room: allRooms[i]
+     * }
+     * from index.js
+     */
+    $("#main").load("showRole", {id: socket.id, room: data.room});
 })
--- server/index.js
+++ server/index.js
@@ -12,8 +12,13 @@ app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use("/static", express.static(path.resolve(__dirname + '/../client/static/')));
 
 
-var users = new Array();
-var rooms = {}; //dictionary or map
+var allUsers = new Array();
+var allRooms = new Map();
+
+// a helper function to generate random integers [0, max)
+function getRandomInt(max) { 
+    return Math.floor(Math.random() * max);
+}
 
 //===============small descriptions of user and room "objects"===========
 /*user = {
@@ -22,11 +27,20 @@ var rooms = {}; //dictionary or map
     room: String, room id the user is in,
 }
 
-rooms[i] is a list of users in ith room
-rooms[i] = {
+allRooms[i] is a list of users in ith room
+allRooms[i] = {
     id: String, 6 digits
     roomhost: String, socket-id of the person who created it
-    members: List, users in this room
+    members: List[Object], users in this room (refer below)
+    roundsDistribution: List[int], number of members going for a round
+    spiesCount: int, number of spies
+}
+
+allRooms[i].members[j] = {
+    username: String, name of a player
+    id: String, socketID
+    role: boolean, true if a player is spy, false otherwise
+    friends: List[String], list of names of players with the same role
 }
 
 */
@@ -34,65 +48,170 @@ rooms[i] = {
 
 //===============Socket IO=====================
 io.on('connection', function (socket) {
-    console.log("user connected!");
+    console.log("socket/client connected!");
 
     socket.on("create room", (data) => {
-        rooms[data.room] = {
+        allRooms[data.room] = {
             id: data.room,
             roomhost: socket.id,
-            members: []
+            members: [],
+            roundsDistribution: [],
+            spiesCount: 0
         };
-        users.push(
+        allUsers.push(
             {
                 id: socket.id, 
                 username: data.username, 
                 room: data.room
             }
         )
-        rooms[data.room].members.push({
+        allRooms[data.room].members.push({
             username: data.username,
-            id: data.room
+            id: socket.id,
+            role: false, // role = true -> spy, role = false -> resistance
+            friends: [] //List[String], list of names of people with the same role
         });
         socket.join(data.room);
-        io.in(data.room).emit('room', {room: rooms[data.room]});
+
+        io.in(data.room).emit('room', {room: allRooms[data.room]});
     })
 
-    socket.on("join room", (data)=>{
-        users.push(
-            {
-                id: socket.id, 
-                username: data.username, 
-                room: data.room
-            }
-        )
-        if(!(data.room in rooms)){
+    socket.on("join room", (data) => {
+        console.log("joining the room=" + data.room)
+        if (allRooms[data.room].members.length > 10) {
+            //todo: implement notification of full room
+            console.log("join room event for a full room");
+            exit();
+        }
+        if(!(data.room in allRooms)){
+            //todo: implement notification of non-existing room
             console.log("join room event for non-existing room!");
             exit();
         }
-        rooms[data.room].members.push({
+        allUsers.push(
+            {
+                id: socket.id, 
+                username: data.username, 
+                room: data.room
+            }
+        )
+        
+        allRooms[data.room].members.push({
             username: data.username,
-            id: socket.id
+            id: socket.id,
+            role: false, // role = true -> spy, role = false -> resistance
+            friends: [] //List[String], list of names of people with the same role
         });
+
         socket.join(data.room);
-        io.in(data.room).emit('room', {room: rooms[data.room]});
+        io.in(data.room).emit('room', {room: allRooms[data.room]});
     })
 
-    socket.on("disconnect", ()=>{
-        console.log("User disconnected!");
-        var currentUser = users.find((user) => {
+    socket.on("start", () => {
+        console.log("the game started! <index.js>")
+        var roomID = "", found = false;
+        for (var i = 0; i < allUsers.length; i++) {
+            if (allUsers[i].id.localeCompare(socket.id) == 0) {
+                roomID = allUsers[i].room;
+                found = true;
+                break;
+            }
+        }
+        if (!found) { // if room is not found
+            console.log("Room not found error!")
+            exit(-1);
+        }
+        /**
+         * When start button is pressed we assign roles
+         * Initial assignment of roles happens when the number of members
+         * reaches 5 people
+         */
+        const playerCount = allRooms[roomID].members.length;
+        if (playerCount == 5) {
+            allRooms[roomID].spiesCount = 2;
+            allRooms[roomID].roundsDistribution[0] = 2;
+            allRooms[roomID].roundsDistribution[1] = 3;
+            allRooms[roomID].roundsDistribution[2] = 2;
+            allRooms[roomID].roundsDistribution[3] = 3;
+            allRooms[roomID].roundsDistribution[4] = 3;
+        }
+        else if (playerCount == 6) {
+            allRooms[roomID].spiesCount = 2;
+            allRooms[roomID].roundsDistribution[0] = 2;
+            allRooms[roomID].roundsDistribution[1] = 3;
+            allRooms[roomID].roundsDistribution[2] = 4;
+            allRooms[roomID].roundsDistribution[3] = 3;
+            allRooms[roomID].roundsDistribution[4] = 4;
+        }
+        else if (playerCount == 7) { // from 7 people and more, 4th round is a star round(requires two fails)
+            allRooms[roomID].spiesCount = 3;
+            allRooms[roomID].roundsDistribution[0] = 2;
+            allRooms[roomID].roundsDistribution[1] = 3;
+            allRooms[roomID].roundsDistribution[2] = 3;
+            allRooms[roomID].roundsDistribution[3] = 4;
+            allRooms[roomID].roundsDistribution[4] = 4;
+        }
+        else if (playerCount == 8 || playerCount == 9) {
+            allRooms[roomID].spiesCount = 3;
+            allRooms[roomID].roundsDistribution[0] = 3;
+            allRooms[roomID].roundsDistribution[1] = 4;
+            allRooms[roomID].roundsDistribution[2] = 4;
+            allRooms[roomID].roundsDistribution[3] = 5;
+            allRooms[roomID].roundsDistribution[4] = 5;
+        }
+        else {
+            allRooms[roomID].spiesCount = 4;
+            allRooms[roomID].roundsDistribution[0] = 3;
+            allRooms[roomID].roundsDistribution[1] = 4;
+            allRooms[roomID].roundsDistribution[2] = 4;
+            allRooms[roomID].roundsDistribution[3] = 5;
+            allRooms[roomID].roundsDistribution[4] = 5;
+        }
+        var i = 0, randIndex;
+        while (i < allRooms[roomID].spiesCount) {
+            randIndex = getRandomInt(playerCount);
+            if (!allRooms[roomID].members[randIndex].role) { //if player is not a spy
+                allRooms[roomID].members[randIndex].role = true; //we make him a spy
+                i++;
+            }
+        }
+        for (var i = 0; i < allRooms[roomID].members.length; i++) {
+            if (allRooms[roomID].members[i].role == true) { //if a player is spy
+                for (var j = 0; j < allRooms[roomID].members.length; j++) {
+                    if (allRooms[roomID].members[j].role == true && i != j) {
+                        allRooms[roomID].members[i].friends.push(allRooms[roomID].members[j].username);
+                    }
+                }
+            }
+            else {
+                for (var j = 0; j < allRooms[roomID].members.length; j++) {
+                    if (allRooms[roomID].members[j].role == false && i != j) {
+                        allRooms[roomID].members[i].friends.push(allRooms[roomID].members[j].username);
+                    }
+                }
+            }
+        }
+        console.log(allRooms[roomID].members[0].friends);
+        io.in(roomID).emit("gameStart", {room: allRooms[roomID]});
+    })  
+
+    socket.on("disconnect", () => {
+        //todo: implement allRooms[i].members list filtering
+        console.log("socket/client disconnected!");
+        var currentUser = allUsers.find((user) => {
             return user.id === socket.id;
         })
-        users = users.filter((user)=>{ 
+        allUsers = allUsers.filter((user)=>{ 
             return user.id !== socket.id;
         });
         //we have to delete the user from the room if the user existed in some specific room
-        if(currentUser){
+        if (currentUser) {
             //todo: check if the user is the host of the room
             console.log("user", currentUser.username, "disconnected from his room", currentUser.room);
-            rooms[currentUser.room].members = rooms[currentUser.room].members.filter((user) =>{
+            allRooms[currentUser.room].members = allRooms[currentUser.room].members.filter((user) =>{
                 return user.id !== socket.id;
             });
-            io.in(currentUser.room).emit('room', {room: rooms[currentUser.room]});
+            io.in(currentUser.room).emit('room', {room: allRooms[currentUser.room]});
         }
     })
 })
@@ -104,7 +223,7 @@ app.get("/", (req, res) =>{
 })
 
 app.get("/game", (req, res) =>{
-    res.render(("game"), {users: users});
+    res.render(("game"), {users: allUsers});
 })
 
 
@@ -112,6 +231,10 @@ app.get("/game", (req, res) =>{
 app.post("/room", (req, res) => {
     // console.log(req.body);
     res.render('room', {room: req.body.room, user: req.body.user});
+})
+
+app.post("/showRole", (req, res) => {
+    res.render('showRole', {id: req.body.id, room: req.body.room});
 })
 
 //===============Server LISTENS=================
